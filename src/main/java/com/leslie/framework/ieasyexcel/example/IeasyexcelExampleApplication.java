ce0e023ee6f224e0754e6b2aabdbfec364526aa0
leslie <leslie.hudy@gmail.com>
2021-06-06 23:04:14 +0800

    init

---
+++ src/main/java/com/leslie/framework/ieasyexcel/example/IeasyexcelExampleApplication.java
@@ -0,0 +1,13 @@
+package com.leslie.framework.ieasyexcel.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeasyexcelExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeasyexcelExampleApplication.class, args);
+    }
+
+}
